8a942f8d7844ca4c35bffb16c1d02692f2a93626
Brian Salemi <briandsalemi@gmail.com>
2019-05-30 00:23:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @receiver.balance += amount
       self.status = "complete"
     else
-      @status = "rejected"
+      self.status = "rejected"
     "Transaction rejected. Please check your account balance."
     end
   end
